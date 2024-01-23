75cda509e143fa53db2615a7c705208ef2df1b44
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-01-23 13:54:21 +0100

    Code done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        int biggestPalindom = najdiNejvetsiPalindrom();
+        System.out.println(biggestPalindom);
+    }
+
+    private static int najdiNejvetsiPalindrom() {
+        int biggestPalindrom = 0;
+
+        for (int i = 999; i >= 100; i--) {
+            for (int j = 999; j >= 100; j--) {
+                int soucin = i * j;
+
+                if (isPalindrom(soucin) && soucin > biggestPalindrom) {
+                    biggestPalindrom = soucin;
+                }
+            }
         }
+
+        return biggestPalindrom;
+    }
+
+    private static boolean isPalindrom(int cislo) {
+        String cisloStr = String.valueOf(cislo);
+        String obraceneCisloStr = new StringBuilder(cisloStr).reverse().toString();
+        return cisloStr.equals(obraceneCisloStr);
     }
 }
